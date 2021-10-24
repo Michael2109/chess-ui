72f25bb43ec794ccdfcc7290b8850fac3262c767
Michael <michael.jhaywoods@gmail.com>
2021-10-24 16:37:35 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "react-bootstrap";
+import SockJS from "sockjs-client";
+import { Stomp } from '@stomp/stompjs';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +21,30 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="primary" onClick={connect}>
+          Test
+        </Button>
       </header>
+
+
     </div>
   );
 }
+function connect(){
+  const socket = new SockJS('http://127.0.0.1:8080/chess-websocket');
+
+  const stompClient = Stomp.over(socket);
+  stompClient.connect({}, function (frame: unknown) {
+
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/topic/greetings', function (greeting) {
+      console.log("Successfully subscribed")
+      console.log(greeting.body)
+    });
+
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': "Michael"}));
+  });
+}
+
 
 export default App;
